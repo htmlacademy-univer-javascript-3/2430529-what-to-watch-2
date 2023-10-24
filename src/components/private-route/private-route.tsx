83ea424730591b661982986ed743a9bdd1a14497
Tatiana <tanya18082000@mail.ru>
2023-10-24 11:35:24 +0300

    fix: private router

--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -1,9 +1,10 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, ReactElement } from 'react';
 import { AppRoute, AuthorizationStatus } from '../../const';
 import { Navigate } from 'react-router-dom';
 
 type Props = {
   authorizationStatus: AuthorizationStatus;
+  children: ReactElement;
 };
 
 export default function PrivateRoute({
