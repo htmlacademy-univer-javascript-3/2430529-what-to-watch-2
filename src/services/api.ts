4499df52d7421826d8050766365b40ac78aa19d0
Tatiana <tanya18082000@mail.ru>
2023-12-08 09:40:01 +0300

    fix: api services

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,26 +1,5 @@
-import axios, {
-  AxiosError,
-  AxiosInstance,
-  AxiosRequestConfig,
-  AxiosResponse,
-} from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import { getToken } from './token';
-import { StatusCodes } from 'http-status-codes';
-import { toast } from 'react-toastify';
-
-type DetailMessageType = {
-  type: string;
-  message: string;
-};
-
-const StatusCodeMapping: Record<number, boolean> = {
-  [StatusCodes.BAD_REQUEST]: true,
-  [StatusCodes.UNAUTHORIZED]: true,
-  [StatusCodes.NOT_FOUND]: true,
-};
-
-const shouldDisplayError = (response: AxiosResponse) =>
-  !!StatusCodeMapping[response.status];
 
 const BASE_URL = 'https://13.design.pages.academy';
 const REQUEST_TIMEOUT = 5000;
@@ -40,19 +19,6 @@ export const createAPI = (): AxiosInstance => {
 
     return config;
   });
-
-  api.interceptors.response.use(
-    (response) => response,
-    (error: AxiosError<DetailMessageType>) => {
-      if (error.response && shouldDisplayError(error.response)) {
-        const detailMessage = error.response.data;
-
-        toast.warn(detailMessage.message);
-      }
-
-      throw error;
-    }
-  );
 
   return api;
 };
